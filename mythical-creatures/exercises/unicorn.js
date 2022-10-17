00cf85a06a879d526cb3e859050e3bd353f6ff68
John Ammon <ammonj78@gmail.com>
2022-10-17 17:27:23 -0600

    Adds unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,29 @@
 class Unicorn {
   constructor(name, color) {
-    this.name = name;
-    this.color = color;
+    // console.log(name)
+    // console.log(color);
+    this.name = name
+    if(color === undefined){
+      this.color = 'white';
+    } else {
+      this.color = color;
+    }
   }
+
+  isWhite(){
+    if(this.color !== 'white'){
+      return false
+    }
+  }
+
+says(words){
+  return '**;* ' + words + ' *;**'
+}
 }
 
 module.exports = Unicorn;
+
+//1. read the test
+//2. ask yourself if its going to fail and why.
+//3. run the test
+//4. make it pass
