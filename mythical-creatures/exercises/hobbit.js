aceb823bef986ef25748d47b42b1a73e51b90e4c
John Ammon <ammonj78@gmail.com>
2022-10-20 14:03:13 -0600

    Progres on hobit

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,18 +1,31 @@
 
 class Hobbit {
-  constructor(name,age){
-    this.name = name
+  constructor(objectHobbit){
+    this.name = objectHobbit.name
     this.age = 0
-
+    this.adult = false
+    this.hasRing = false
+    this.old = false
   }
 
-  function nameToUpper() {
-    this.name[0].toUpperCase()
-    return nameToUpper()
-  }
+    celebrateBirthday(){
+      return this.age++
+    }
 
+    timeTravel(age,name){
+    if
+    (this.age === 32) {
+      this.adult = false
+}
 
+    if(this.age === 33) {
+      this.adult = true
+    }
 
+    if(this.age >= 101) {
+      this.adult = false
+    }
+}
 }
 
 
