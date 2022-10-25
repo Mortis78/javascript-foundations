22d76fcc5d2a91082bb3b0f87b3e3d6c0fd3209e
John Ammon <ammonj78@gmail.com>
2022-10-25 17:54:27 -0600

    compleats pirate file/test

--- mythical-creatures/exercises/pirate.js
+++ mythical-creatures/exercises/pirate.js
@@ -1,13 +1,41 @@
 class Pirate{
-  constructor(){
+  constructor(name,job){
 
-
-
-
-
+    this.name = name
+    this.job = job || 'scallywag'
+    this.cursed = false
+    this.booty = 0
 
   }
+//things to do
+
+  robShip(){
+
+    if(this.booty === 500){
+      this.cursed = true
+      return 'ARG! I\'ve been cursed!'
+
+    }else {
+      this.booty +=100
+      return 'YAARRR!'
+    }
+    }
+
+  liftCurse(){
+    if (this.cursed = true && this.booty === 500){
+      this.booty = 200
+      this.cursed = false
+      return 'Your curse has been lifted!'
+    }else{
+      // this.cursed = false
+      return 'You don\'t need to lift a curse!'
+    }
+  }
 }
+
+
+
+
 
 
 module.exports = Pirate
