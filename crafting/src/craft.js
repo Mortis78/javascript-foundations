a71b519ff0a5761e721d7cb69e7f5b2ed5015330
John Ammon <ammonj78@gmail.com>
2022-11-09 15:44:22 -0700

    Compleats crafting

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,3 +1,20 @@
+class Craft{
+    constructor(newCraft){
+        this.name = newCraft.type
+        this.materials = newCraft.materials
+        this.completed = false
+    }
+    completeCraft(){
+        this.completed = true
+        return 'All done! It looks great!'
+    }
+    calculateProjectTotal(){
+        var totalCost = 0
+        for(var i = 0; i < this.materials.length; i ++){
+            totalCost += this.materials[i].amount *  this.materials[i].price          
+        }
+        return totalCost
+    }
 
-
+}
 module.exports = Craft;
